b9e8dd44027caff81d9900693694f1c6d2a03fef
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-16 23:33:12 +0000

    modifie HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,3 +7,29 @@ import TheWelcome from '../components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+<template>
+  <div>
+    <!-- Navigation -->
+    <NavBar />
+    
+    <!-- Hero Section -->
+    <HeroSection />
+    
+    <!-- Product Section -->
+    <ProductSection />
+    
+    <!-- Featured Logos -->
+    <FeaturedLogos />
+    
+    <!-- Location Section -->
+    <LocationSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import ProductSection from '@/components/ProductSection.vue'
+import FeaturedLogos from '@/components/FeaturedLogos.vue'
+import LocationSection from '@/components/LocationSection.vue'
+</script>
