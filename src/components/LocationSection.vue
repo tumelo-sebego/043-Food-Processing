205cbd9b576afdab0136c40f8a7868afc551c746
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-16 23:42:45 +0000

    modifie HomeView.vue

---
+++ src/components/LocationSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="location-section py-5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <h2 class="location-title mb-4">
+            STOP BY OUR<br>
+            KANSAS CITY<br>
+            LOCATION
+          </h2>
+          <p class="fs-5 mb-4">
+            Experience the authentic taste of Jones BBQ at our original 
+            Kansas City location where it all started.
+          </p>
+          <button class="btn btn-outline-light btn-lg">
+            GET DIRECTIONS
+          </button>
+        </div>
+        <div class="col-lg-6">
+          <div class="text-center">
+            <img 
+              src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&fit=crop" 
+              alt="BBQ Restaurant Interior" 
+              class="img-fluid rounded-3 shadow-lg"
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+// Location section component
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
 <template>
   <div>
     <!-- Navigation -->
