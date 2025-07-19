6185704aaca57c00296dba474d81ad9529a130c4
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-19 14:36:13 +0000

    menu now appears on top of content on mobile screens

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,34 +5,34 @@
         <i class="bi bi-fire me-2"></i>
         JONES
       </a>
-      
-      <button 
-        class="navbar-toggler" 
-        type="button" 
-        data-bs-toggle="collapse" 
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      
+
       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">HOME</a>
+            <a class="nav-link text-black" href="#">HOME</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">OUR STORY</a>
+            <a class="nav-link text-black" href="#">OUR STORY</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">WHAT</a>
+            <a class="nav-link text-black" href="#">WHAT</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">PRESS</a>
+            <a class="nav-link text-black" href="#">PRESS</a>
           </li>
         </ul>
-        
+
         <div class="d-flex align-items-center nav-right">
-          <i class="bi bi-cart text-white me-3" style="font-size: 1.2rem;"></i>
+          <i class="bi bi-cart text-black me-3" style="font-size: 1.2rem;"></i>
           <button class="btn btn-outline-light btn-sm">
             SHOP SAUCES
           </button>
@@ -45,3 +45,20 @@
 <script setup lang="ts">
 // Navigation component
 </script>
+
+<style scoped>
+@media (max-width: 991.98px) {
+  .navbar-collapse {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    background-color: var(--jones-red);
+    padding: 1rem;
+    z-index: 1000;
+  }
+  .navbar {
+    position: relative;
+  }
+}
+</style>
