5141409359b183b8bfdb79677c22750ac4f4427c
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-07-22 05:53:37 +0200

    replaced place holder featured logos to actual logos with reference

--- src/components/FeaturedLogos.vue
+++ src/components/FeaturedLogos.vue
@@ -7,27 +7,45 @@
       <div class="row align-items-center justify-content-center">
         <div class="col-12 col-md-2 text-center mb-3">
           <div class="fw-bold text-uppercase" style="color: var(--jones-red); font-size: 1.2rem">
-            FEAST
+            <a
+              href="https://startupmag.co.za/2021/06/how-siphelele-zekani-found-success-with-his-043-sweet-garlic-sauce/"
+            >
+              <img src="../assets/images/f_logo1.png" alt="Bon Appetit" class="img-fluid" />
+            </a>
           </div>
         </div>
         <div class="col-12 col-md-2 text-center mb-3">
           <div class="fw-bold text-uppercase" style="color: var(--jones-red); font-size: 1.2rem">
-            EATER
+            <a
+              href="https://www.dailydispatch.co.za/lifestyle/2020-06-20-hot-stuff-ec-entrepreneurs-sauce-takes-off/"
+            >
+              <img src="../assets/images/f_logo2.png" alt="Bon Appetit" class="img-fluid" />
+            </a>
           </div>
         </div>
         <div class="col-12 col-md-2 text-center mb-3">
           <div class="fw-bold text-uppercase" style="color: var(--jones-red); font-size: 1.2rem">
-            VANITY FAIR
+            <a
+              href="https://www.sowetanlive.co.za/sebenza-live/2021-06-08-zekani-enjoys-sweet-success-with-garlic-sauce/"
+            >
+              <img src="../assets/images/f_logo3.png" alt="Bon Appetit" class="img-fluid" />
+            </a>
           </div>
         </div>
         <div class="col-12 col-md-2 text-center mb-3">
           <div class="fw-bold text-uppercase" style="color: var(--jones-red); font-size: 1.2rem">
-            WSJ
+            <a href="https://www.vukuzenzele.gov.za/sipheleles-sauce-success">
+              <img src="../assets/images/f_logo4.png" alt="Bon Appetit" class="img-fluid" />
+            </a>
           </div>
         </div>
         <div class="col-12 col-md-2 text-center mb-3">
           <div class="fw-bold text-uppercase" style="color: var(--jones-red); font-size: 1.2rem">
-            THRILLIST
+            <a
+              href="https://www.pressreader.com/south-africa/post-south-africa/20220720/281586654327634"
+            >
+              <img src="../assets/images/f_logo5.png" alt="Bon Appetit" class="img-fluid" />
+            </a>
           </div>
         </div>
       </div>
